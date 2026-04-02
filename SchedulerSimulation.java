03d15699ba9a30d14ccb8070d924d7b7642929f3
Fatimah1010 <444052271@std.psau.edu.sa>
2026-04-02 18:26:27 +0300

    Feature 3: Added waiting time calculation and reporting

--- SchedulerSimulation.java
+++ SchedulerSimulation.java
@@ -29,15 +29,25 @@ class Process implements Runnable {
     private int burstTime; // Total time the process requires to complete (in milliseconds)
     private int timeQuantum; // Time slice (time quantum) allowed per CPU access (in milliseconds)
     private int remainingTime; // Time left for the process to finish its execution
+    
     private int priority;// Feature 1: Process Priority
-// Constructor to initialize the process with name, burst time, and time quantum
+
+// Feature 3: Waiting Time Tracking
+   private long creationTime;
+   private long finishTime;
+
+
+
+    // Constructor to initialize the process with name, burst time, and time quantum
 public Process(String name, int burstTime, int timeQuantum) {
         this.name = name;
         this.burstTime = burstTime;
         this.timeQuantum = timeQuantum;
         this.remainingTime = burstTime; // Initially, remaining time is equal to the burst time
         this.priority = (int)(Math.random() * 5) + 1; 
+        this.creationTime = System.currentTimeMillis();
     }
+
 
     // This method will be called when the thread for this process is started
     @Override
@@ -72,6 +82,7 @@ public Process(String name, int burstTime, int timeQuantum) {
         }
         
         remainingTime -= runTime; // Deduct the run time from the remaining time
+        this.finishTime = System.currentTimeMillis();
         int overallProgress = (int) (((double)(burstTime - remainingTime) / burstTime) * 100);
         String overallProgressBar = createProgressBar(overallProgress, 20);
         
@@ -146,6 +157,9 @@ public Process(String name, int burstTime, int timeQuantum) {
     public boolean isFinished() {
         return remainingTime <= 0;
     }
+    public long getWaitingTime() {
+    return (finishTime - creationTime) - burstTime;
+} 
 }
 
 public class SchedulerSimulation {
@@ -284,6 +298,14 @@ static int contextSwitches = 0;// Feature 2: Context Switch Counter
                           "╚════════════════════════════════════════════════════════════════════════════════╝" + 
                           Colors.RESET + "\n");
                           System.out.println("Total context switches: " + contextSwitches);
+                          System.out.println("\nProcess Summary:");
+        System.out.println("Name\tBurst\tWaiting");
+
+for (Process p : processMap.values()) {
+    System.out.println(p.getName() + "\t" +
+        p.getBurstTime() + "\t" +
+        p.getWaitingTime());
+}
     }
     
     // Method to add a process to the queue and map, while printing a "ready" message
